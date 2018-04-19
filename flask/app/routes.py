301159791fca97206a916f13c1f0915ed423a160
klingj3 <Gobosox09!github>
2018-04-19 09:42:48 -0400

    Progress made on Flask displaying data, still not processing.

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -2,6 +2,11 @@ from flask import render_template, flash, redirect, request
 from app import app
 from app.forms import UrlSearchForm
 
+import sys, os
+sys.path.append(os.path.abspath('../utilities'))
+import reddit_toolkit as rt
+
+
 @app.route('/', methods=['GET','POST'])
 def search():
     search = UrlSearchForm()
@@ -14,6 +19,9 @@ def find_results(url):
     results = []
     url_string = url.data['url']
 
-    # Todo: Call functions which return list of results.
+    results = rt.flask_packaging(url_string)
+    for result in results:
+        print(result['title'], file=sys.stderr)
+
 
     return render_template('results.html', results=results, url=url_string)
