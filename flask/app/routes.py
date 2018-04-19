f8f04c700f3f3500d7b41a88a4d964534a1c6d6d
John Klingelhofer <klingj3@rpi.edu>
2018-04-19 11:31:13 -0400

    Some entity linking and party affiliation done, integrated with flask.

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -5,7 +5,10 @@ from app.forms import UrlSearchForm
 import sys, os
 sys.path.append(os.path.abspath('../utilities'))
 import reddit_toolkit as rt
+from political_tools import Tagger
 
+# Due to the time required to train the tagger, it's better to have one
+tagger = Tagger(test=False)
 
 @app.route('/', methods=['GET','POST'])
 def search():
@@ -16,12 +19,8 @@ def search():
 
 @app.route('/results')
 def find_results(url):
-    results = []
+
     url_string = url.data['url']
-
-    results = rt.flask_packaging(url_string)
-    for result in results:
-        print(result['title'], file=sys.stderr)
-
+    results = rt.flask_packaging(url_string, tagger)
 
     return render_template('results.html', results=results, url=url_string)
