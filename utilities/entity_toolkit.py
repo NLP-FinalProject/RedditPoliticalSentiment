eee4c3621d40ccdef72a33751262803c377b1923
nathanpotolsky <nathanpotolsky@gmail.com>
2018-05-01 19:22:34 -0400

    Complied methods into EntityLinker class.  Added Load/Save capability for class dictionary.

--- utilities/entity_toolkit.py
+++ utilities/entity_toolkit.py
@@ -17,6 +17,7 @@ import requests
 import sys
 import wikipedia
 import urllib3
+import json
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Load/generate requisite nltk files
@@ -29,128 +30,170 @@ except LookupError:
     nltk.download('punkt')
     tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
-""" ADDTIONAL ENTITY TOOLS WITH THE PROF API, NOT TESTED OR INTEGRATED"""
+    """ ADDTIONAL ENTITY TOOLS WITH THE PROF API, NOT TESTED OR INTEGRATED"""
 
 
-def identify_entity(sentence):
-    """
-    :param sentence:
-    :return: A list of entities and events in the order they appear in the sentence
-    """
-    serviceurl = 'https://blender04.cs.rpi.edu/~jih/cgi-bin/ere.py'
-    payload = {'textcontent': sentence}
-    # verify=False makes it so a HTTPS certificate is not required for the request
-    r = requests.get(serviceurl, params=payload, verify=False)
+class EntityLinker():
 
-    parsed_html = BeautifulSoup(r.text, "html.parser")
-    # We are only concerned about the div lines as that's where the entities and events are in the HTML
-    mentions = parsed_html.findAll('div')
+    def __init__(self, *, path='entity_files/dict.json'):
+        self.path = path
 
-    # Our list of entities and events
-    taggedMentions = []
+        # If file exists
+        if os.path.isfile(path):
+            # Load json file into dictionary
+            self.load_json(path)
 
-    # For each mention in sentence
-    for i in mentions:
-        # If it is an entity like "George Bush"
-        if (str(i).startswith("<div id=\"d")):
-            entityID = re.sub("(.+Entity ID: )|(<br/>.+)", "", str(i))
-            entityMention = re.sub("(.+Entity Mention: )|(<br/>.+)", "", str(i))
-            entityMentionType = re.sub("(.+Entity Mention Type: )|(<br/>.+)", "", str(i))
-            entityType = re.sub("(.+Entity Type: )|(<br/>.+)", "", str(i))
-            entityClass = re.sub("(.+Entity Class: )|(<br/>.+)", "", str(i))
-            taggedMentions.append(['Entity', entityID, entityMention, entityMentionType, entityType, entityClass])
-        # If it is an event like "meet"
-        elif (str(i).startswith("<div id=\"e")):
-            eventID = re.sub("(.+Event ID: )|(<br/>.+)", "", str(i))
-            trigger = re.sub("(.+Trigger: )|(<br/>.+)", "", str(i))
-            eventType = re.sub("(.+Event Type: )|(<br/>.+)", "", str(i))
-            eventSubtype = re.sub("(.+Event Subtype: )|(<br/>.+)", "", str(i))
-            genericity = re.sub("(.+Genericity: )|(<br/>.+)", "", str(i))
-            modality = re.sub("(.+Modality: )|(<br/>.+)", "", str(i))
-            polarity = re.sub("(.+Polarity: )|(<br/>.+)", "", str(i))
-            tense = re.sub("(.+Tense: )|(<br/>.+)", "", str(i))
-            arguments = re.sub("(.+Arguments: )|(<br/>.+)", "", str(i))
-            person = re.sub("(.+;\">)|(</a>.+)", "", str(i))
-            taggedMentions.append(['Event', eventID, trigger, eventType, eventSubtype, genericity, modality, polarity, tense, arguments, person])
-    return taggedMentions
+        # If parent folder exists
+        elif os.path.isdir((os.path.dirname(path))):
+            self.ent_dict = {}
+            # Save dictionary to json file
+            self.save_json(path)
 
-""" END UNTESTED ENTITIY TOOLS"""
+        # If parent folder doesn't exist
+        else:
+            sys.stderr.write("ERROR: PATH '" + path + "' NOT FOUND")
+            exit()
 
+    def load_json(self, path='entity_files/dict.json'):
+        """
+        :param json_dict:
+        :action: Saves dictionary to json file
+        """
+        self.ent_dict = json.load(open(path))
 
-def get_all_entity_political_parties(self, entity_list):
-    """
-    :param entityList: the list that is returned from identify_entity when passed an input String
-    :return: a list of tuples containing the normalized name for the entity and their party
-    """
-    entity_party_list = []
-    for e in entity_list:
-        entity_party_list.append(entity_to_political_party(e[2]))
-    return entity_party_list
+    def save_json(self, path='entity_files/dict.json'):
+        """
+        :param json_dict:
+        :action: Loads json file into dictionary
+        """
+        with open(path, 'w') as outfile:
+            json.dump(self.ent_dict, outfile)
 
+    def pretty_print_json(self, json_dict):
+        """
+        :param json_dict:
+        :action: Pretty prints json to console
+        """
+        print(json.dumps(json_dict, indent=4, sort_keys=True))
 
-def page_title_to_political_party(title):
-    """
-    :param wiki: a valid WikipediaPage object
-    :return: A string representing the political party or affiliation of the entity described in the wikipage, if
-            available. Otherwise, a string 'None' is returned.
-    """
-    # Go through wikipedia json to get the id for wikidata
-    resp = requests.get(url='https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageprops&titles=' + title)
-    data = resp.json()
-    page_data = data['query']['pages'][list(data['query']['pages'].keys())[0]]
-    page_properties = page_data['pageprops']
-    item_id = page_properties['wikibase_item']
+    def identify_entity(sentence):
+        """
+        :param sentence:
+        :return: A list of entities and events in the order they appear in the sentence
+        """
+        serviceurl = 'https://blender04.cs.rpi.edu/~jih/cgi-bin/ere.py'
+        payload = {'textcontent': sentence}
+        # verify=False makes it so a HTTPS certificate is not required for the request
+        r = requests.get(serviceurl, params=payload, verify=False)
 
-    # With item id in tow, extract political affiliation
-    client = Client()
-    entity = client.get(item_id, load=True)
-    try:
-        party_entity = entity.getlist(client.get('P102'))[0]
-        return str(party_entity.label)
-    except:
-        return 'None found'
+        parsed_html = BeautifulSoup(r.text, "html.parser")
+        # We are only concerned about the div lines as that's where the entities and events are in the HTML
+        mentions = parsed_html.findAll('div')
 
+        # Our list of entities and events
+        taggedMentions = []
 
-def entity_to_political_party(entity, type='Person', previous_subject_titles=[]):
-    """
-    :param entity: String containing the name of the entity to be passed
-    :return: A tuple containing the name of the matching page and that page's affiliation
-    """
-    pages = wikipedia.search(entity)
-    # With the exception of Morrissey and Madonna, people have two words in their names
-    if type =='Person':
-        page_titles = [p.split() for p in pages]
-        page_titles = [[w for w in title if '(' not in w] for title in page_titles]
-        page_titles = [' '.join(title) for title in page_titles if len(title) >= 2]
-    else:
-        sys.stderr.write("ERROR: Only person entity-type supported")
-        return None
+        # For each mention in sentence
+        for i in mentions:
+            # If it is an entity like "George Bush"
+            if (str(i).startswith("<div id=\"d")):
+                entityID = re.sub("(.+Entity ID: )|(<br/>.+)", "", str(i))
+                entityMention = re.sub("(.+Entity Mention: )|(<br/>.+)", "", str(i))
+                entityMentionType = re.sub("(.+Entity Mention Type: )|(<br/>.+)", "", str(i))
+                entityType = re.sub("(.+Entity Type: )|(<br/>.+)", "", str(i))
+                entityClass = re.sub("(.+Entity Class: )|(<br/>.+)", "", str(i))
+                taggedMentions.append(['Entity', entityID, entityMention, entityMentionType, entityType, entityClass])
+            # If it is an event like "meet"
+            elif (str(i).startswith("<div id=\"e")):
+                eventID = re.sub("(.+Event ID: )|(<br/>.+)", "", str(i))
+                trigger = re.sub("(.+Trigger: )|(<br/>.+)", "", str(i))
+                eventType = re.sub("(.+Event Type: )|(<br/>.+)", "", str(i))
+                eventSubtype = re.sub("(.+Event Subtype: )|(<br/>.+)", "", str(i))
+                genericity = re.sub("(.+Genericity: )|(<br/>.+)", "", str(i))
+                modality = re.sub("(.+Modality: )|(<br/>.+)", "", str(i))
+                polarity = re.sub("(.+Polarity: )|(<br/>.+)", "", str(i))
+                tense = re.sub("(.+Tense: )|(<br/>.+)", "", str(i))
+                arguments = re.sub("(.+Arguments: )|(<br/>.+)", "", str(i))
+                person = re.sub("(.+;\">)|(</a>.+)", "", str(i))
+                taggedMentions.append(['Event', eventID, trigger, eventType, eventSubtype, genericity, modality, polarity, tense, arguments, person])
+        return taggedMentions
 
-    # If any of the results have been previously discussed in the thread, those should be given priority
-    new_titles = [title for title in page_titles if title not in previous_subject_titles]
-    page_titles = previous_subject_titles + new_titles
-
-    # Iterate through these titles
-    for title in page_titles:
-        found_party = page_title_to_political_party(title)
-        if found_party != 'None found':
-            return title, found_party
-    return 'No political figure', 'None found'
+    """ END UNTESTED ENTITIY TOOLS"""
 
 
-def political_party_to_value(party):
-    """
-    :param party: A string representing the name of a political party
-    :return: A value [-1.0, 1.0] representing this affiliation.
-    """
-    # TODO: More nuanced approach, use wikipedia API rather than fixed values
-    if 'republican' in party.lowercase():
-        return 1
-    elif 'democrat' in party.lowercase():
-        return -1
-    else:
-        sys.stderr.write('ERROR: Method not yet completed.\n Cannot handle ' + party)
-        return 0
+    def get_all_entity_political_parties(self, entity_list):
+        """
+        :param entityList: the list that is returned from identify_entity when passed an input String
+        :return: a list of tuples containing the normalized name for the entity and their party
+        """
+        entity_party_list = []
+        for e in entity_list:
+            entity_party_list.append(entity_to_political_party(e[2]))
+        return entity_party_list
+
+
+    def page_title_to_political_party(title):
+        """
+        :param wiki: a valid WikipediaPage object
+        :return: A string representing the political party or affiliation of the entity described in the wikipage, if
+                available. Otherwise, a string 'None' is returned.
+        """
+        # Go through wikipedia json to get the id for wikidata
+        resp = requests.get(url='https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageprops&titles=' + title)
+        data = resp.json()
+        page_data = data['query']['pages'][list(data['query']['pages'].keys())[0]]
+        page_properties = page_data['pageprops']
+        item_id = page_properties['wikibase_item']
+
+        # With item id in tow, extract political affiliation
+        client = Client()
+        entity = client.get(item_id, load=True)
+        try:
+            party_entity = entity.getlist(client.get('P102'))[0]
+            return str(party_entity.label)
+        except:
+            return 'None found'
+
+
+    def entity_to_political_party(entity, type='Person', previous_subject_titles=[]):
+        """
+        :param entity: String containing the name of the entity to be passed
+        :return: A tuple containing the name of the matching page and that page's affiliation
+        """
+        pages = wikipedia.search(entity)
+        # With the exception of Morrissey and Madonna, people have two words in their names
+        if type =='Person':
+            page_titles = [p.split() for p in pages]
+            page_titles = [[w for w in title if '(' not in w] for title in page_titles]
+            page_titles = [' '.join(title) for title in page_titles if len(title) >= 2]
+        else:
+            sys.stderr.write("ERROR: Only person entity-type supported")
+            return None
+
+        # If any of the results have been previously discussed in the thread, those should be given priority
+        new_titles = [title for title in page_titles if title not in previous_subject_titles]
+        page_titles = previous_subject_titles + new_titles
+
+        # Iterate through these titles
+        for title in page_titles:
+            found_party = page_title_to_political_party(title)
+            if found_party != 'None found':
+                return title, found_party
+        return 'No political figure', 'None found'
+
+    def political_party_to_value(party):
+        """
+        :param party: A string representing the name of a political party
+        :return: A value [-1.0, 1.0] representing this affiliation.
+        """
+        # TODO: More nuanced approach, use wikipedia API rather than fixed values
+        if 'republican' in party.lowercase():
+            return 1
+        elif 'democrat' in party.lowercase():
+            return -1
+        else:
+            sys.stderr.write('ERROR: Method not yet completed.\n Cannot handle ' + party)
+            return 0
 
 
 class Tagger(ChunkParserI):
@@ -270,3 +313,8 @@ class Tagger(ChunkParserI):
                 last = i
         names = [' '.join([g[0] for g in group]) for group in groups[1:]]
         return names
+
+dankMeme = EntityLinker()
+
+
+print("\nCompleted")
