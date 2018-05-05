e246ef386b88ded33fd6b748cf42d48e966a27cb
klingj3 <Gobosox09!github>
2018-05-05 14:47:17 -0400

    Error corretions, alterations to use of professor's API for entity recognition.

--- utilities/flask_interface.py
+++ utilities/flask_interface.py
@@ -1,14 +1,13 @@
-from urllib.error import URLError
 from collections import Counter
 from json.decoder import JSONDecodeError
-from nltk import sent_tokenize
 from utilities.api_keys import *
+from nltk.corpus import stopwords
+from urllib.error import URLError
 from utilities.reddit_toolkit import RedditExplorer
 from utilities.sentiment_toolkit import SentimentClassifier
 
 import utilities.entity_toolkit as et
 
-junk_words = {'guys', 'one', 'center', 'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than'}
 
 # Interface between flask and the core of this project.
 
@@ -17,6 +16,7 @@ class Interface(object):
         self.rt = RedditExplorer(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
         self.ent_linker = et.EntityLinker(path=abs_path+'saved_data/entity_files/dict.json')
         self.sentiment = SentimentClassifier(load_path=abs_path + 'trained_models/model.tfl')
+        self.stop_words = set(stopwords.words("english"))
 
     def flask_packaging(self, *, url, max_number=5, num_top_com=3):
         """
@@ -34,11 +34,12 @@ class Interface(object):
             """
             pass
 
-        def heuristic_for_comment(comment, score, parent_party=0):
+        def heuristic_for_comment(comment, score, entities, parent_party=0):
             val = 0
-            nps = self.ent_linker.get_continuous_chunks(comment)[:-1]
+
             try:
-                affiliations = [self.ent_linker.entity_to_political_party(np) for np in nps]
+                affiliations = [self.ent_linker.entity_to_political_party(entity=entity, ent_type=ent_type)
+                                for entity, ent_type in entities]
                 affiliations = [x for x in affiliations if x is not None]
             except (ConnectionError, URLError, JSONDecodeError) as e:
                 return 0
@@ -62,49 +63,61 @@ class Interface(object):
         for submission in submissions[:max_number]:
             sub = self.rt.parse_submission_info(submission)
             sub['top_comments'] = []
-            for comment in self.rt.top_comments(submission.comments, num_top_com):
-                comm = {
-                        'words': comment.body.split(' '),
-                        'score': comment.score,
-                        'url': comment.permalink,
-                        # We'll seed it with these keys, partially because they're sometimes missed
-                        # as they can be adjectives as well as nouns.
-                        'r_words': {'republican', 'republicans', 'gop'},
-                        'l_words': {'democrats', 'democrat', 'dems'},
-                        'sentiment': self.sentiment.predict(comment.body)
-                        }
-                nps = self.ent_linker.get_continuous_chunks(comment.body)[:-1]
-                # We'll ignore thing that are upper-case too, partially because they're more likely to be
-                # false-positives, but also because people who type in caps aren't contributing to the conversation.
-                affiliations = [(np, self.ent_linker.entity_to_political_party(np)) for np in nps if
-                                self.ent_linker.entity_to_political_party(np) is not None and np != np.upper()]
+            all_comments = self.rt.all_comments_to_list(submission.comments, max_num_comments=100)
 
-                print(affiliations)
+            # In some subreddits, comment scores are hidden for the first several hours. We can't analyze
+            # these, so they aren't returned in all comments.
+            if len(all_comments) > 0:
+                comment_entities = self.ent_linker.identify_all_entities(all_comments)
+                comment_entities = self.remove_non_political_entities(comment_entities)
+                for i, comment in enumerate(self.rt.top_comments(submission.comments, num_top_com)):
+                    comm = {
+                            'words': comment.body.split(' '),
+                            'score': comment.score,
+                            'url': comment.permalink,
+                            'sentiment': self.sentiment.predict(comment.body)
+                            }
 
-                for np, affiliation in affiliations:
-                    if "Republican Party" == affiliation[1]:
-                        for word in affiliation[0].lower().split(' '):
-                            comm['r_words'].add(word.lower())
-                        comm['r_words'].add(np.lower())
-                    if "Democratic Party" == affiliation[1]:
-                        for word in affiliation[0].lower().split(' '):
-                            comm['l_words'].add(word.lower())
-                        comm['l_words'].add(np.lower())
-                sub['top_comments'].append(comm)
+                    entities = comment_entities[i]
 
-            # We limit the number of response comments for the sake of reducing computational complexity.
-            for comment, score in self.rt.all_comments_to_list(submission.comments, max_num_comments=100):
-                mod = heuristic_for_comment(comment, score)
-                if mod > 0:
-                    sub['r_percentage'] += mod
-                else:
-                    sub['l_percentage'] += abs(mod)
+                    # We'll ignore thing that are upper-case too, partially because they're more likely to be
+                    # false-positives, but also because people who type in caps aren't contributing to the conversation.
+                    affiliations = [(entity, self.ent_linker.entity_to_political_party(entity=entity, ent_type=ent_type)) for
+                                    entity, ent_type in entities if
+                                    self.ent_linker.entity_to_political_party(entity=entity, ent_type=type) is not None
+                                    and entity != entity.upper()]
 
-            total = (sub['r_percentage']+sub['l_percentage'])
-            if total != 0:
-                sub['r_percentage'] = sub['r_percentage']/total*100
-                sub['l_percentage'] = sub['l_percentage']/total*100
+                    for entity, affiliation in affiliations:
+                        if "Republican Party" == affiliation[1]:
+                            for word in affiliation[0].lower().split(' '):
+                                sub['r_words'].add(word.lower())
+                            for word in entity.lower().split(' '):
+                                sub['r_words'].add(word.lower())
+                        if "Democratic Party" == affiliation[1]:
+                            for word in affiliation[0].lower().split(' '):
+                                sub['l_words'].add(word.lower())
+                            for word in entity.lower().split(' '):
+                                sub['l_words'].add(word.lower())
+                    sub['top_comments'].append(comm)
+
+                # We limit the number of response comments for the sake of reducing computational complexity.
+                for i, comm_tup in enumerate(all_comments):
+                    comment, score = comm_tup
+                    mod = heuristic_for_comment(comment, score, comment_entities[i])
+                    if mod > 0:
+                        sub['r_percentage'] += mod
+                    else:
+                        sub['l_percentage'] += abs(mod)
+
+                total = (sub['r_percentage']+sub['l_percentage'])
+                if total != 0:
+                    sub['r_percentage'] = sub['r_percentage']/total*100
+                    sub['l_percentage'] = sub['l_percentage']/total*100
 
             discussions.append(sub)
 
         return discussions
+
+    def remove_non_political_entities(self, all_entities):
+        return [[tup for tup in comment_entities if tup[0].lower() not in self.stop_words
+                and not self.sentiment.in_dictionary(tup[0])] for comment_entities in all_entities]
